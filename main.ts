e3c3981560549790c545537bde4221d42167a45e
jafadgex <92938346+jafadgex@users.noreply.github.com>
2021-11-11 00:35:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+let mySprite2 = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f e e f f c c . 
+    f f f f f e e f f c c f . 
+    f f f b f e e f b f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite2, 100, 100)
+tiles.setTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleBlueCrystal)
+scene.cameraFollowSprite(mySprite2)
+info.startCountdown(20)
